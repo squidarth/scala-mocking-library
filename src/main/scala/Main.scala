008641c5fcd8091ac516d5045f071a571b39e8e6
Sid Shanker <sshanker220@gmail.com>
2020-09-27 21:23:18 -0400

    Some cleanup.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,38 +4,17 @@ import scala.reflect.runtime.universe._
 import shapeless._
 import shapeless.ops.hlist
 
-//  object Macros {
-//    def hello(s: String): Unit = macro helloImpl
-//    def helloImpl(c: blackbox.Context)(s: c.Expr[String]): c.Expr[Unit] = {
-//      import c.universe._
-//      c.Expr(q"""println("hello" + ${s.tree})""")
-//    }
-//  }
-
 class Foo3 {
   def fooify(s: Int) = s + 3
 }
 
-
-
 object Main extends App with MockMatchers {
-  println("Hello")
-  val mock = MockMacros.mock[Foo3]
-  println(mock.fooify(4))
-  println(mock)
-//
-//println(fooifyResult)
-//val mock = MockMacros.mock[Foo3]
-//        val func = (mock.fooify _)
-//        func.k
-//        when(4)
-//
- //val mock = MockMacros.mock[Foo3]
-//val mock = MockMacros.mock[Foo3]
- //println(mock.mockStuff)
-// val funcObject = (mock.fooify _).when(3)
-// println(mock.fooify(2))
-// //println(mock.fooify(_).when(9))
-// val stubObject = MockMacros.when(mock)
- //println(stubObject.fooify(10))
+  val fooObject = new Foo3
+
+
+val mock = MockMacros.mock[Foo3]
+//println(mock.fooify(10))
+MockMacros.when(mock.fooify(7)).thenReturn(200)
+println(mock.fooify(7))
+
 }
--- src/main/scala/Mock.scala
+++ src/main/scala/Mock.scala
@@ -1,95 +1,62 @@
 import scala.language.experimental.macros
-import scala.Nothing
 import scala.reflect.macros.whitebox
 import scala.reflect.runtime.universe._
-import scala.collection.mutable.{Seq => MutableSeq} 
+
+class MockUndefinedException(s:String) extends Exception(s)
 
 class MockContext {
   var handlers : Seq[Any] = Seq[Any]()
 
-  var currentMockMethod: (String, Any) = null 
+  var currentMockMethod: (Mock[_], String, Any) = null 
 
   def appendHandler[T](value: T) = {
     val fullCall = currentMockMethod match {
-      case (methodName, arg) => (methodName, arg, value)
+      case (mock, methodName, arg) => (mock, methodName, arg, value)
     }
     handlers = handlers ++ Seq(fullCall)
   }
+
+  def setCurrentMockMethod(mock: Mock[_], funcName: String, arg: Any) = {
+    currentMockMethod = (mock, funcName, arg)
+  }
+
+  def findMatchingHandler(mock: Mock[_], funcName: String, arg: Any): Option[Any] = {
+    handlers.collect { handler =>
+      handler match {
+        case (savedMock, savedFunctionName, savedArg, value) if mock == savedMock && funcName == savedFunctionName && arg == savedArg => 
+          value
+      }
+    }.headOption
+  }
 }
 
-trait Mock[T] {
-}
-
-
-
+trait Mock[T]
 
 trait MockMatchers {
   import scala.language.implicitConversions
 
   implicit val mockContext = new MockContext
-
-//  protected def * = new MatchAny
-//  protected implicit def toMockParameter[T](v: T) = new MockParameter(v)
-//  protected implicit def matcherBaseToMockParameter[T](m: MatcherBase) = new MockParameter[T](m)
-//  protected implicit def stubFunction[A, V](f: Function1[A,V]) = f.asInstanceOf[FakeFunction[A,V]]
-
-}
-
-class ArgumentMatcher(template: Product) extends Function1[Product, Boolean] {
-  
-  def apply(args: Product) = template == args
-  
-  override def toString = template.productIterator.mkString("(", ", ", ")")
-}
-
-class FunctionAdapter1[T1, R](f: T1 => R) extends Function1[Product, R] {
-  
-  def apply(args: Product) = {
-    assert(args.productArity == 1)
-    f(args.productElement(0).asInstanceOf[T1])
-  }
-}
-
-class FakeFunction[A, V] extends Function1[A, V] {
-    val returnVal: Option[V] = None
-
-//    def when(v1: MockParameter[A]) = println("when called 1")
-//    def when(matcher: FunctionAdapter1[A, Boolean]) = println("when called")
-   
-    def apply(v1: A) : V = returnVal match {
-        case Some(v) => v.asInstanceOf[V]
-        case None => throw new Exception("Undefined mock value")
-    }
 }
 
 class Stubbing[T](implicit val mockContext: MockContext) {
   def thenReturn(returnVal: T) = {
-    mockContext.handlers
-
+    mockContext.appendHandler(returnVal)
   }
 }
 
 object MockMacros {
   import scala.language.experimental.macros
   import scala.language.implicitConversions
-//  def when[T](mock: Mock[T]) : T = macro whenImpl[T]
-//
-//  def whenImpl[T: c.WeakTypeTag](c: blackbox.Context)(mock: c.Expr[Mock[T]]): c.Expr[T] = {
-//    import c.universe._
-//
-//    val mockingType = weakTypeOf[T]
-//
-//    val result = q"""new ${mockingType.resultType}() {
-//        override def fooify(s: Int) = s + 6 + ${mock}.mockStuff
-//    }"""
-//    c.Expr(result)
-//  }
 
-  def when[T](returnVal: T)(implicit mockContext: MockContext): Stubbing[T] = {
+  def when[T](getReturnVal: => T)(implicit mockContext: MockContext): Stubbing[T] = {
+    try {
+      getReturnVal
+    } catch {
+      case e: MockUndefinedException => ()
+      case e: Throwable => throw e
+    }
     new Stubbing[T]()
   }
-
-
 
   def mock[T](implicit mockContext: MockContext) : T with Mock[T] = macro mockImpl[T]
   def mockImpl[T: c.WeakTypeTag](c: whitebox.Context)(mockContext: c.Expr[MockContext]): c.Expr[T] = {
@@ -113,10 +80,16 @@ object MockMacros {
       }
       
       val name = method.name
+      val firstParamName = method.paramLists.headOption.flatMap(_.headOption).map { symbol => symbol.name}.get
 
       q"""
       override def ${name.toTermName}(...${paramsString}) : ${returnType} = {
-         throw new Exception("hello") 
+        ${mockContext}.setCurrentMockMethod(this, ${name.toString()}, ${firstParamName.toTermName})
+        val foundHandler = ${mockContext}.findMatchingHandler(this, ${name.toString()}, ${firstParamName.toTermName})
+        foundHandler match {
+          case Some(value) => value.asInstanceOf[${returnType}]
+          case None => throw new MockUndefinedException("no mock found")
+        }
       }
       """
     }.toList
